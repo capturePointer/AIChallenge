4be9da17cf2ca803941f55cb209df909f54de8d5
Marco Lopez <marcolm485@gmail.com>
2016-11-16 10:37:16 -0600

    Add R file to properly calculate file acoustic properties

--- audio_processing.py
+++ audio_processing.py
@@ -11,17 +11,31 @@ import pyaudio
 import wave
 import numpy as np
 import csv
+import os
 
-audio = importr('warbleR')
+"""
+import rpy2.robjects as robjects
+r_source = robjects.r['source']
+r_source('/path_to_file/myfunc.R')
+r_getname = robjects.globalenv['getname']
+"""
+
+r_source = ro.r['source']
+r_source(os.getcwd() + '/sound.R')
+specan3 = ro.globalenv['specan3']
+warbler = importr('warbleR')
+tuner = importr('tuneR')
+seewave = importr('seewave')
 
 def get_file_attributes(filename):
 	d = {'start': ro.r(0), 'end': ro.r(5), "sound.files": filename, "selec": ro.r(0)}
 	inp = ro.DataFrame(d)
-	x = audio.specan(inp, bp = ro.IntVector((0,22)), wl = 2048, threshold = 5, parallel = 1)
+	x = specan3(inp, bp = ro.IntVector((0,22)), wl = 2048, threshold = 5, parallel = 1)
 	data_list = x[3:]
 	raw_data = []
 	print(x)
-	for data in data_list:
-		raw_data.append(data[0])
+	for (i, data) in enumerate(data_list):
+		if i is not 12:
+			raw_data.append(data[0])
 	return raw_data
 	#print(raw_data)
