df11d65a12c0c0ad7b86c30a1638210c3aca47aa
Marco Lopez <marcolm485@gmail.com>
2016-11-18 17:20:50 -0600

    Start classifying samples

--- gui.py
+++ gui.py
@@ -1,37 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from Tkinter import *
+from pygame import mixer
+from pygame.mixer import music
+from prediction import get_model, get_file_attributes
 import ttk
 
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
-    except ValueError:
-        pass
+def play_sample(filename):
+	print("Playing " + filename)
+	mixer.init()
+	music.load(filename)
+	music.play(0)
+
+def classify(label_text, filename, mainframe):
+	clf = get_model('voice_recognition.pkl')
+	attributes = get_file_attributes(filename.get())
+	x = clf.predict([attributes])
+	if x == [0]:
+		x = "Male"
+	else:
+		x = "Female"
+	label_text.set(x)
+	mainframe.update_idletasks()
     
 root = Tk()
-root.title("Feet to Meters")
+root.title("Voice gender classifier")
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe = ttk.Frame(root, padding="12 12 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
-
-feet = StringVar()
-meters = StringVar()
-
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
-
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
-
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
-
-feet_entry.focus()
-root.bind('<Return>', calculate)
+filename = StringVar()
+gender = StringVar()
+ttk.Button(mainframe, text="Play sample Nº 1", command=lambda:play_sample('lana.wav')).grid(column=0, row=0, sticky=W)
+ttk.Button(mainframe, text="Play sample Nº 2", command=lambda:play_sample('corey.wav')).grid(column=0, row=1, sticky=W)
+ttk.Button(mainframe, text="Play sample Nº 3", command=lambda:play_sample('stephen.wav')).grid(column=0, row=2, sticky=W)
+ttk.Button(mainframe, text="Play sample Nº 4", command=lambda:play_sample('emily.wav')).grid(column=0, row=3, sticky=W)
+ttk.Button(mainframe, text="Play sample Nº 5", command=lambda:play_sample('mia.wav')).grid(column=0, row=4, sticky=W)
+ttk.Radiobutton(mainframe, text="Sample Nº 1", variable=filename, value='lana.wav').grid(column=3, row=0, sticky=E)
+ttk.Radiobutton(mainframe, text="Sample Nº 2", variable=filename, value='corey.wav').grid(column=3, row=1, sticky=E)
+ttk.Radiobutton(mainframe, text="Sample Nº 3", variable=filename, value='stephen.wav').grid(column=3, row=2, sticky=E)
+ttk.Radiobutton(mainframe, text="Sample Nº 4", variable=filename, value='emily.wav').grid(column=3, row=3, sticky=E)
+ttk.Radiobutton(mainframe, text="Sample Nº 5", variable=filename, value='mia.wav').grid(column=3, row=4, sticky=E)
+ttk.Button(mainframe, text="Make sample classification", command=lambda:classify(gender, filename, mainframe)).grid(column=3, row=5, sticky=W)
+ttk.Label(mainframe, text= "Gender: ").grid(column=2, row=6, sticky=E)
+ttk.Label(mainframe, textvariable=gender).grid(column=3, row=6, sticky=E)
 
 root.mainloop()
--- prediction.py
+++ prediction.py
@@ -1,12 +1,46 @@
+from numpy import *
+import scipy as sp
+from pandas import *
+import pandas.rpy.common as com
+from rpy2.robjects.packages import importr
+import rpy2.robjects as ro
+import sys
 import cPickle
+from scipy.stats import kurtosis, skew, entropy
+from sklearn import svm
+import pyaudio
+import wave
+import numpy as np
+import csv
+import os
+
+r_source = ro.r['source']
+r_source(os.getcwd() + '/sound.R')
+specan3 = ro.globalenv['specan3']
+warbler = importr('warbleR')
+tuner = importr('tuneR')
+seewave = importr('seewave')
+
+def get_file_attributes(filename):
+	d = {'start': ro.r(0), 'end': ro.r(5), "sound.files": filename, "selec": ro.r(0)}
+	inp = ro.DataFrame(d)
+	x = specan3(inp, bp = ro.IntVector((0,22)), wl = 2048, threshold = 5, parallel = 1)
+	data_list = x[3:]
+	raw_data = []
+	print(x)
+	for (i, data) in enumerate(data_list):
+		if i is not 12:
+			raw_data.append(data[0])
+	return raw_data
 
 def get_model(model_name):
 	with open(model_name, 'rb') as fid:
 		clf = cPickle.load(fid)
 	return clf
 
-
+"""
 if __name__ == "__main__":
 	clf = get_model('voice_recognition.pkl')
 	print(clf.predict([[0.0597809849598081,0.0642412677031359,0.032026913372582,0.0150714886459209,0.0901934398654331,0.0751219512195122,12.8634618371626,274.402905502067,0.893369416700807,0.491917766397811,0,0.0597809849598081,0.084279106440321,0.0157016683022571,0.275862068965517,0.0078125,0.0078125,0.0078125,0,0]]))
 	print(clf.predict([[0.165508946001837,0.0928835369116316,0.183043922369765,0.0700715015321757,0.250827374872319,0.180755873340143,1.70502911922022,5.76911536636857,0.938829422236203,0.601528810198165,0.267701736465781,0.165508946001837,0.185606931233589,0.0622568093385214,0.271186440677966,0.227022058823529,0.0078125,0.5546875,0.546875, 0.035]]))
+"""
